e454e89b9cd20310135727996f6630b0c033e4d9
xgebi <sarah@sarahgebauer.com>
2018-05-31 20:59:53 +0200

    Very WIP, I'm stuck and I need to sleep on this

---
+++ src/templating/TemplateEngine.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template handler
+ * 
+ * @author Sarah Gebauer
+ * @version 0.0.1
+ */
+namespace SlothAdminApi\Templating;
+
+
+/**
+ * @package SlothAdminApi\Templating
+ */
+class TemplateEngine {
+
+  private $entries;
+  
+  function __construct($entries, $themePath) {
+    // TODO locate theme
+
+    // 
+  }
+  
+}
---
+++ src/templating/templateHandler.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template handler
+ * 
+ * @author Sarah Gebauer
+ * @version 0.0.1
+ */
+namespace SlothAdminApi\Templating;
+
+
+/**
+ * @package SlothAdminApi\Templating
+ */
+class TemplateHandler {
+
+  private $entries;
+  
+  function __construct($entries) {
+
+  }
+  
+}
