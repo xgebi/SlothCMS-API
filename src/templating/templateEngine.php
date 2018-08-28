6bc0c7029fbf7462441e442200c6bfc97bf546cc
gebauer <sarah@sarahgebauer.com>
2018-08-28 18:08:27 +0200

    WIP

--- src/templating/templateEngine.php
+++ src/templating/templateEngine.php
@@ -16,13 +16,29 @@ class TemplateEngine {
   private $entries;
   
   function __construct($entries, $themePath) {
+    $themeInfo = json_decode(file_get_contents($themePath . "info.json"));
+
     // 1. loop through entries
-    foreach ($variable as $key => $value) {
-      // a. in each entry loop through languages
-        // I. locate proper template for a post type
-          // i. when template is missing use index.toe
-        // II. in each language generate an html file
-      // b. store post type, tags and categories
+    foreach ($entries as $entry) {
+      // a. locate proper template for a post type
+        $templateToUse = "";
+        foreach ($themeInfo->templates->specialTemplates as $postType) {
+          if ($postType->postTypeName == $entry->postType) {
+            $templateToUse = $postType->template;
+            break;
+          }
+        }
+        // I. when template is missing use index.toe
+        if (strlen($templateToUse) == 0) {
+          $templateToUse = "index.toe";
+        }
+      // b. in each language generate an html file
+      foreach ($entry->languages as $language) {
+        
+
+        // I. store post type, tags and categories per language
+        
+      }
     }
     // 2. regenerate home page
       // a. in each entry loop through languages
