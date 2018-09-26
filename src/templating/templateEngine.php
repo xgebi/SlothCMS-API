0cacb07c824392e4de1edc2a2da0d534143c1230
gebauer <sarah@sarahgebauer.com>
2018-09-26 18:05:12 +0200

    prep-work

--- src/templating/templateEngine.php
+++ src/templating/templateEngine.php
@@ -14,8 +14,13 @@ namespace SlothAdminApi\Templating;
 class TemplateEngine {
 
   private $entries;
+  private $mainLanguage;
+  private $themePath;
   
-  function __construct($entries, $themePath) {
+  function __construct($entries, $themePath, $mainLanguage, $homePageRenewal = false) {
+    $this->mainLanguage = $mainLanguage;
+    $this->$themePath = $themePath
+
     $themeInfo = json_decode(file_get_contents($themePath . "info.json"));
 
     // 1. loop through entries
@@ -40,8 +45,14 @@ class TemplateEngine {
         
       }
     }
+
     // 2. regenerate home page
+    if ($homePageRenewal) {      
       // a. in each entry loop through languages
+      foreach ($entry->languages as $language) {
+        $this->loadTemplate('home', $language);
+      }
+    }
     // 3. if post type has archive
       // a. in each entry loop through languages
         // I. regenerate archive
@@ -51,6 +62,19 @@ class TemplateEngine {
     // 5. if post type has tags
       // a. in each entry loop through languages
         // I. regenerate tags
+  }
+
+  private function loadTranslatedTemplate($templateName, $language) {
+    if (strcasecmp($language, $this->mainLanguage) == 0) {
+      // load template.toe
+      if (is_file($this->themePath . "/" . $templateName . ".toe")) {
+
+      } else {
+        // load index.toe
+      }
+    } else {
+      // load template-xx.toe
+    }
   }
   
 }
