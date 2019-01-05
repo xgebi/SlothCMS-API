5e6e351cab3ff545e1aa7b86f40aa6898508790d
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-01-05 21:53:31 +0100

    Sloth 9 WIP, Sloth 11 started

--- src/services/TemplateService.php
+++ src/services/TemplateService.php
@@ -11,13 +11,27 @@ namespace slothcms\services;
 
 class TemplateService {
     private $template;
+    private $args;
+
+    private static $OPENING_TAG = "<#";
+    private static $CLOSING_TAG = "#>";
 
     /**
      * TemplateService constructor.
      * @param $template
+     * @param $args
      */
-    public function __construct($template) {
+    public function __construct($template, $args) {
         $this->template = $template;
+
+        $this->processTemplate($template);
+    }
+
+    private function processTemplate($template) {
+        $openingTagPosition = -1;
+        while (($openingTagPosition = strpos($template, self::$OPENING_TAG)) > 0) {
+            $closingTagPosition = strpos($template, self::$CLOSING_TAG, $openingTagPosition);
+        }
     }
 
 
